fbc30a2e7a82b83d78eac860ef84dce4d93a01f8
Dachae <no3yho2@naver.com>
2023-11-16 23:37:49 +0900

    [front] skeleton-components

--- front/final_pjt_front/src/views/MovieDetailView.vue
+++ front/final_pjt_front/src/views/MovieDetailView.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <h1>무비 디테일 뷰</h1>
+    <h3>한줄평</h3>
+    <h4>작성</h4>
+    <CommentCreate />
+    <h4>조회</h4>
+    <CommentList />
   </div>
 </template>
 
 <script setup>
-
+  import CommentCreate from '@/components/CommentCreate.vue'
+  import CommentList from '@/components/CommentList.vue'
 </script>
 
 <style scoped>
